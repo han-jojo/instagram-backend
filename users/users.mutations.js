7f0bd47dc0a5151c5e83e6e1bfdcb6abecac9c68
Jiho Han <mlpzaq123456@naver.com>
2021-04-02 19:16:33 +0900

    4.4 login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -38,9 +38,30 @@ export default {
             password: uglyPassword,
           },
         });
-      } catch(e) {
+      } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      //args.username의 user를 찾아라
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
+
+      //args.password의 비밀번호를 검사하라
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+
+      //둘 다 통과시 user에게 token을 발행한다
     },
   },
 };
