57f805c57a61c7209e2b76db59d24469cb393eb3
Jiho Han <mlpzaq123456@naver.com>
2021-04-02 15:29:51 +0900

    4.0 Create Account

---
+++ users/users.mutations.js
@@ -0,0 +1,38 @@
+import client from "../client";
+import bcrypt from "bcrypt";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      //username이나 email이 이미 DB에 존재하는지 확인한다
+      const exitingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(exitingUser);
+      // hash password
+      const uglyPassword = await bcrypt.hash(password, 10);
+      // save and return the user
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
+        },
+      });
+    },
+  },
+};
