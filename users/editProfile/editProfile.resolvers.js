74f833cfb236d8a5bc1cef24b15136ef511f9b2d
Jiho Han <mlpzaq123456@naver.com>
2021-04-05 19:57:29 +0900

    4.16 File Upload 3
    Node.js 버전 호환 틀림으로 인한 버그 수정
    
    npx npm-force-resolutions
    
    npm i

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -7,7 +7,11 @@ const resolverFn = async (
   { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser }
 ) => {
-  console.log(avatar);
+  
+  const { filename, createReadStream } = await avatar;
+  const stream = createReadStream();
+  console.log(stream);
+  
   let uglyPassword = null;
   if (newPassword) uglyPassword = await bcrypt.hash(newPassword, 10);
 
