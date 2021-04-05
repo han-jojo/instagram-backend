c3fbb481d87f6c0811ab50c9e98229d3de36a70c
Jiho Han <mlpzaq123456@naver.com>
2021-04-05 15:43:26 +0900

    4.9 Authentication 2
    token을 HTTP header에 삽입했다

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import schema from "./schema";
 
 const server = new ApolloServer({
   schema,
+  context: {
+    token: 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE3NjA0ODQ0fQ.HQVSD6ByZy7L4ozAHCeKRLSmKhWDsAkv_Bjl-UOLELQ"
+  },
 });
 
 const PORT = process.env.PORT;
