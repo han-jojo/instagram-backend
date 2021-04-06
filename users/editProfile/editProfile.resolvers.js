728211210457ecad4c4b7a2402afcb21d7e92a56
Jiho Han <mlpzaq123456@naver.com>
2021-04-06 12:07:27 +0900

    4.19 Changing Avatar
    파일 업로드 및 프로필사진 URL로 변경기능 구현

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,6 @@
-import client from "../../client";
-import bcrypt from "bcrypt";
 import { createWriteStream } from "fs";
+import bcrypt from "bcrypt";
+import client from "../../client";
 import { protectedResolver } from "../users.utils";
 
 const resolverFn = async (
@@ -8,21 +8,22 @@ const resolverFn = async (
   { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser }
 ) => {
-  
-  const { filename, createReadStream } = await avatar;
-  const readStream = createReadStream();
-
-  //aws 부분
-  const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
-  readStream.pipe(writeStream);
-  //aws 부분
-
-  console.log(readStream);
-  
+  let avatarUrl = null;
+  if (avatar) {
+    const { filename, createReadStream } = await avatar;
+    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+    const readStream = createReadStream();
+    const writeStream = createWriteStream(
+      process.cwd() + "/uploads/" + newFilename
+    );
+    readStream.pipe(writeStream);
+    avatarUrl = `http://localhost:4000/static/${newFilename}`;
+  }
   let uglyPassword = null;
-  if (newPassword) uglyPassword = await bcrypt.hash(newPassword, 10);
-
-  const updateUser = await client.user.update({
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
     where: {
       id: loggedInUser.id,
     },
@@ -33,10 +34,10 @@ const resolverFn = async (
       email,
       bio,
       ...(uglyPassword && { password: uglyPassword }),
+      ...(avatarUrl && { avatar: avatarUrl }),
     },
   });
-
-  if (updateUser.id) {
+  if (updatedUser.id) {
     return {
       ok: true,
     };
@@ -47,6 +48,7 @@ const resolverFn = async (
     };
   }
 };
+
 export default {
   Mutation: {
     editProfile: protectedResolver(resolverFn),
