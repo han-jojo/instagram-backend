0e036644e03d423021687da7314ce6a74dc44598
Jiho Han <mlpzaq123456@naver.com>
2021-04-08 17:21:24 +0900

    6.3 Upload photo 2
    정규식 패턴을 통해 String 내에 있는 특정한 String을 추출하도록 도와준다

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,3 +1,4 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
@@ -6,9 +7,28 @@ export default {
       async (_, { file, caption }, { loggedInUser }) => {
         if (caption) {
           //캡션을 검사한다
+          const hashtags = caption.match(/#[\w]+/g);
           //해쉬태그를 생성하거나 가져온다
         }
         //생성된 해쉬태그와 함게 사진을 저장한다
+        client.photo.create({
+          data: {
+            file,
+            caption,
+            hashtags: {
+              connectOrCreate: [
+                {
+                  where: {
+                    hashtags: "#food",
+                  },
+                  create: {
+                    hashtag: "#food",
+                  },
+                },
+              ],
+            },
+          },
+        });
         //해쉬태그에 사진을 저장한다
       }
     ),
