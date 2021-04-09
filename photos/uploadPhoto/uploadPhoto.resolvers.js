12bf935829085fe921040ac1d52f980729558a97
Jiho Han <mlpzaq123456@naver.com>
2021-04-09 22:22:39 +0900

    6.4 Upload photo 3
    hashtagsObj를 새로 선언한다

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -5,28 +5,30 @@ export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
+
         if (caption) {
           //캡션을 검사한다
           const hashtags = caption.match(/#[\w]+/g);
           //해쉬태그를 생성하거나 가져온다
         }
         //생성된 해쉬태그와 함게 사진을 저장한다
-        client.photo.create({
+        return client.photo.create({
           data: {
             file,
             caption,
-            hashtags: {
-              connectOrCreate: [
-                {
-                  where: {
-                    hashtags: "#food",
-                  },
-                  create: {
-                    hashtag: "#food",
-                  },
-                },
-              ],
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
             },
+            ...loggedInUser(
+              hashtagObj.length > 0 && {
+                hashtags: {
+                  connectOrCreate: hashtagObj,
+                },
+              }
+            ),
           },
         });
         //해쉬태그에 사진을 저장한다
