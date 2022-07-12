203e2c9a818a35aedbad8c8e3c8e8bf1b36ad857
Jiho Han <mlpzaq123456@naver.com>
2022-07-12 23:11:28 +0900

    [Reset] 2022.07 백엔드 프로젝트 리팩토링

--- src/photos/photos.utils.js
+++
@@ -1,7 +0,0 @@
-export const processHashtags = (caption) => {
-  const hashtags = caption.match(/#[\w]+/g) || [];
-  return hashtags.map((hashtag) => ({
-    where: { hashtag },
-    create: { hashtag },
-  }));
-};
