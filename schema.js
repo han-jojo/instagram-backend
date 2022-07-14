34584dce1bd8fa2f9c6812e280edc2bb8dfb119e
Jiho Han <mlpzaq123456@naver.com>
2022-07-14 22:55:15 +0900

    [update] User Schame, typeDefs,resolvers 리팩토링

--- schema.js
+++ schema.js
@@ -5,8 +5,13 @@ import {
   mergeTypeDefs,
 } from "graphql-tools";
 
+/*
+__filename 은 현재 실행 중인 파일 경로
+__dirname 은 현재 실행 중인 폴더 경로
+*/
+
 const typeFiles = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const resolverFiles = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`);
+const resolverFiles = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
 const typeDefs = mergeTypeDefs(typeFiles);
 const resolvers = mergeResolvers(resolverFiles);
