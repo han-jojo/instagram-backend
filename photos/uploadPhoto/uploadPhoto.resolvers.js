2b7e556a0366e8c880f10c5587ac96bda4077795
Jiho Han <mlpzaq123456@naver.com>
2022-07-20 23:41:36 +0900

    [add]Photo Table Model
    Photo Typedefs, Resolvers 추가

---
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -0,0 +1,53 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+import GraphQLUpload from "graphql-upload/GraphQLUpload.js";
+
+export default {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
+        if (caption) {
+          // 내용(caption) 처리
+          // 해쉬태그 처리
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+        }
+
+        let imageUrl = null;
+
+        if (file) {
+          const { createReadStream, filename } = await file;
+          const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+          const readStream = createReadStream();
+          const writeStream = createWriteStream(
+            process.cwd() + "/uploads/" + newFilename
+          );
+          readStream.pipe(writeStream);
+          imageUrl = `http://localhost:4000/images/${newFilename}`;
+        }
+
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
+      }
+    ),
+  },
+  Upload: GraphQLUpload,
+};
